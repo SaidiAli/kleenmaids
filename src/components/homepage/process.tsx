52e467875ee9d3918e76429665acdeb00e559ec9
bonstana <isikosaidiali@gmail.com>
2025-08-20 11:35:52 +0300

    updated process section

--- src/components/homepage/process.tsx
+++ src/components/homepage/process.tsx
@@ -32,12 +32,14 @@ export default function ProcessSection() {
                     <p className="text-gray-600 text-sm">We've made it simple and transparent. Here's exactly how we work with you to deliver the perfect service experience.</p>
                 </div>
 
-                <div className="flex justify-center items-center" id="how-it-works">
+                <div className="relative flex justify-center items-center" id="how-it-works">
+                    <div className="absolute top-10 left-0 right-0 h-0.5 bg-greenish w-[80%] mx-auto">
+                    </div>
                     {process.map((item, index) => (
                         <div className="text-center flex flex-col items-center justify-center" key={index}>
-                            <div className={`relative w-20 h-20 mb-8 rounded-full bg-greenish shadow-xl/30 shadow-greenish flex items-center justify-center transition-all duration-700 transform hover:scale-110 scale-100 opacity-100 animate-pulse-glow group-hover:animate-float`}>
-                                <item.icon className="w-10 h-10 text-white" />
-                            </div>
+                                <div className={`relative w-20 h-20 mb-8 rounded-full bg-greenish shadow-xl/30 shadow-greenish flex items-center justify-center transition-all duration-700 transform hover:scale-110 scale-100 opacity-100 animate-pulse-glow group-hover:animate-float`}>
+                                    <item.icon className="w-10 h-10 text-white" />
+                                </div>
                             <h3 className="text-xl font-semibold mb-4">{item.title}</h3>
                             <p className="text-gray-600 text-lg">
                                 {item.description}
